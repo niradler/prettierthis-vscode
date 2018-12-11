bc847ea05537698b2034bad2bd7b1a1b74df8697
Nir Adler <niradler55@gmail.com>
2018-12-12 00:16:36 +0200

    p json

--- extension.js
+++ extension.js
@@ -5,8 +5,7 @@ const prettier = require("prettier");
 const editor = vscode.window.activeTextEditor;
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-const langs = [{lang:'javascript',ext:"js"}] ;
-const getLang = (languageId) => langs.filter(l=>languageId.includes(l.lang))[0];
+
 function activate(context) {
 
     // Use the console to output diagnostic information (console.log) and errors (console.error)
@@ -18,22 +17,29 @@ function activate(context) {
     // The commandId parameter must match the command field in package.json
     let disposable = vscode.commands.registerCommand('extension.prettierthis', function () {
         // The code you place here will be executed every time your command is executed
-        if (editor) {
+        try {
+            const langs = [{lang:'javascript',ext:"js"}] ;
+            const getLang = (languageId) => langs.filter(l=>languageId.includes(l.lang))[0];
+            if (editor) {
 
-            const {document} = editor;
-            const lang = getLang(document.languageId);
-            console.log(lang)
-            const selection_range = editor["_selections"][0];
-            const text = document.getText(editor.selection);
-            const formated = prettier.format(text, {});
-            const edit = new vscode.WorkspaceEdit();
-            edit.replace(document.uri, selection_range, formated);
-            vscode.window.showInformationMessage('PrettierThis run complete!');
-
-            return vscode
-                .workspace
-                .applyEdit(edit);
+                const {document} = editor;
+                const lang = getLang(document.languageId);
+                console.log(lang)
+                const selection_range = editor["_selections"][0];
+                const text = document.getText(editor.selection);
+                const formated = prettier.format(text, {});
+                const edit = new vscode.WorkspaceEdit();
+                edit.replace(document.uri, selection_range, formated);
+                vscode.window.showInformationMessage('PrettierThis run complete!');
+    
+                return vscode
+                    .workspace
+                    .applyEdit(edit);
+            }
+        } catch (error) {
+            vscode.window.showInformationMessage('PrettierThis: ' + error.message);
         }
+
     });
 
     context.subscriptions.push(disposable);
